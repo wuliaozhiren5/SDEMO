b69453932aa4f0fba38fb8e26ffd18ab077df5a3
wuwenchao <wuwenchao@rr.tv>
2020-08-19 10:40:03 +0800

    add project

---
+++ SDEMO/Pods/Target Support Files/Pods-SDEMO/Pods-SDEMO-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SDEMO : NSObject
+@end
+@implementation PodsDummy_Pods_SDEMO
+@end
---
+++ SDEMO/SDEMO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SDEMO
+//
+//  Created by rrtv on 2020/8/19.
+//  Copyright © 2020 tv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
